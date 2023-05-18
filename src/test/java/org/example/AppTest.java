7a6078c963f9d9a8d2ef624e8c8db73f6f56af1d
k.kirubakaran <k.kirubakaran@stackroute.in>
2023-05-18 06:40:51 +0530

    initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+  
+    @Test
+    public void test1()
+    {
+        System.out.println("Testcase 1 completed");
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("Testcase 2 completed");
+    }
+}
